6280cad1a4a477b4181cd47158e2a9dce5fac113
Erin Hales <mcbridee093@gmail.com>
2019-08-09 09:19:39 -0600

    Got tests running, need to be restructured

--- test/ValidateSPVJs.test.js
+++ test/ValidateSPVJs.test.js
@@ -2,46 +2,41 @@
 /* eslint-disable no-underscore-dangle */
 const BN = require('bn.js');
 
-const ValidateSPV = artifacts.require('ValidateSPVTest');
+const ValidateSPV = require('../contracts-js/ValidateSPV');
 const utils = require('./utils');
+const btcUtils = require('../utils/utils')
 const constants = require('./constants');
 
+const OP_RETURN = constants.OP_RETURN
 
-contract('ValidateSPV', () => {
-  const zeroBN = new BN('0', 10);
 
-  let instance;
-
-  before(async () => {
-    instance = await ValidateSPV.new();
-  });
-
+describe('ValidateSPV', () => {
   describe('#error constants', async () => {
     it('tests the constant getters for that sweet sweet coverage', async () => {
-      let res = await instance.getErrBadLength.call();
-      assert(res.eq(new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 16)));
+      let res = await ValidateSPV.getErrBadLength.call();
+      assert.equal(res, BigInt('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 16));
 
-      res = await instance.getErrInvalidChain.call();
-      assert(res.eq(new BN('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe', 16)));
+      res = await ValidateSPV.getErrInvalidChain.call();
+      assert.equal(res, BigInt('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe', 16));
 
-      res = await instance.getErrLowWork.call();
-      assert(res.eq(new BN('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd', 16)));
+      res = await ValidateSPV.getErrLowWork.call();
+      assert.equal(res, BigInt('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd', 16));
     });
   });
 
   describe('#prove', async () => {
     it('returns true if proof is valid', async () => {
-      const res = await instance.prove(
-        constants.OP_RETURN.TXID_LE,
-        constants.OP_RETURN.INDEXED_HEADERS[0].MERKLE_ROOT_LE,
-        constants.OP_RETURN.PROOF,
-        constants.OP_RETURN.PROOF_INDEX
+      const res = await ValidateSPV.prove(
+        btcUtils.deserializeHex(OP_RETURN.TXID_LE),
+        btcUtils.deserializeHex(OP_RETURN.INDEXED_HEADERS[0].MERKLE_ROOT_LE),
+        btcUtils.deserializeHex(OP_RETURN.PROOF),
+        OP_RETURN.PROOF_INDEX
       );
       assert.isTrue(res);
     });
 
     it('shortcuts the coinbase special case', async () => {
-      const res = await instance.prove(
+      const res = await ValidateSPV.prove(
         constants.OP_RETURN.TXID_LE,
         constants.OP_RETURN.TXID_LE,
         '0x',
@@ -51,7 +46,7 @@ contract('ValidateSPV', () => {
     });
 
     it('returns false if Merkle root is invalid', async () => {
-      const res = await instance.prove(
+      const res = await ValidateSPV.prove(
         constants.OP_RETURN.TXID_LE,
         constants.OP_RETURN.TXID_LE,
         constants.OP_RETURN.PROOF,
@@ -63,7 +58,7 @@ contract('ValidateSPV', () => {
 
   describe('#calculateTxId', async () => {
     it('returns the transaction hash', async () => {
-      const res = await instance.calculateTxId(
+      const res = await ValidateSPV.calculateTxId(
         constants.OP_RETURN.VERSION,
         constants.OP_RETURN.VIN,
         constants.OP_RETURN.VOUT,
@@ -79,11 +74,11 @@ contract('ValidateSPV', () => {
     const compatibilityWSHInput = '0x7bb2b8f32b9ebf13af2b0a2f9dc03797c7b77ccddcac75d1216389abfa7ab37500000000220020eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeffffffff';
     const compatibilityWPKHInput = '0x7bb2b8f32b9ebf13af2b0a2f9dc03797c7b77ccddcac75d1216389abfa7ab37500000000160014eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeffffffff';
     const sequence = new BN('ffffffff', 16);
-    const index = zeroBN;
+    const index = new BN('0', 10);
     const outpointTxId = '0x75b37afaab896321d175acdccd7cb7c79737c09d2f0a2baf13bf9e2bf3b8b27b';
 
     it('returns the tx input sequence and outpoint', async () => {
-      const txIn = await instance.parseInput(input);
+      const txIn = await ValidateSPV.parseInput(input);
 
       assert(txIn._sequence.eq(sequence));
       assert.equal(txIn._hash, outpointTxId);
@@ -92,7 +87,7 @@ contract('ValidateSPV', () => {
     });
 
     it('handles Legacy inputs', async () => {
-      const txIn = await instance.parseInput(legacyInput);
+      const txIn = await ValidateSPV.parseInput(legacyInput);
 
       assert(txIn._sequence.eq(sequence));
       assert.equal(txIn._hash, outpointTxId);
@@ -101,7 +96,7 @@ contract('ValidateSPV', () => {
     });
 
     it('handles p2wpkh-via-p2sh compatibility inputs', async () => {
-      const txIn = await instance.parseInput(compatibilityWPKHInput);
+      const txIn = await ValidateSPV.parseInput(compatibilityWPKHInput);
 
       assert(txIn._sequence.eq(sequence));
       assert.equal(txIn._hash, outpointTxId);
@@ -110,7 +105,7 @@ contract('ValidateSPV', () => {
     });
 
     it('handles p2wsh-via-p2sh compatibility inputs', async () => {
-      const txIn = await instance.parseInput(compatibilityWSHInput);
+      const txIn = await ValidateSPV.parseInput(compatibilityWSHInput);
 
       assert(txIn._sequence.eq(sequence));
       assert.equal(txIn._hash, outpointTxId);
@@ -121,7 +116,7 @@ contract('ValidateSPV', () => {
 
   describe('#parseOutput', async () => {
     it('returns the tx output value, output type, and payload for an OP_RETURN output', async () => {
-      const opReturnTxOut = await instance.parseOutput(
+      const opReturnTxOut = await ValidateSPV.parseOutput(
         constants.OP_RETURN.INDEXED_OUTPUTS[1].OUTPUT
       );
 
@@ -137,7 +132,7 @@ contract('ValidateSPV', () => {
       const value = new BN('1000001000', 10);
       const payload = '0x7849e6bf5e4b1ba7235572d1b0cbc094f0213e6c';
 
-      const wpkhOutput = await instance.parseOutput(output);
+      const wpkhOutput = await ValidateSPV.parseOutput(output);
 
       assert(wpkhOutput._value.eq(value));
       assert(wpkhOutput._outputType.eq(utils.OUTPUT_TYPES.WPKH));
@@ -149,7 +144,7 @@ contract('ValidateSPV', () => {
       const value = new BN('1000000', 10);
       const payload = '0xaedad4518f56379ef6f1f52f2e0fed64608006b3ccaff2253d847ddc90c91922';
 
-      const wshOutput = await instance.parseOutput(output);
+      const wshOutput = await ValidateSPV.parseOutput(output);
 
       assert(wshOutput._value.eq(value));
       assert(wshOutput._outputType.eq(utils.OUTPUT_TYPES.WSH));
@@ -160,9 +155,9 @@ contract('ValidateSPV', () => {
       // Changes 0x6a (OP_RETURN) to 0x7a to create error
       const output = '0x0000000000000000167a14edb1b5c2f39af0fec151732585b1049b07895211';
 
-      const nonstandardOutput = await instance.parseOutput(output);
+      const nonstandardOutput = await ValidateSPV.parseOutput(output);
 
-      assert(zeroBN.eq(nonstandardOutput._value));
+      assert(new BN('0', 10).eq(nonstandardOutput._value));
       assert(nonstandardOutput._outputType.eq(utils.OUTPUT_TYPES.NONSTANDARD));
       assert.isNull(nonstandardOutput._payload);
     });
@@ -172,7 +167,7 @@ contract('ValidateSPV', () => {
       const value = new BN('05dfe8', 16);
       const payload = '0xa654ebafa7a37e04a7ec3f684e34897e48f04962';
 
-      const shOutput = await instance.parseOutput(output);
+      const shOutput = await ValidateSPV.parseOutput(output);
 
       assert(shOutput._value.eq(value));
       assert(shOutput._outputType.eq(utils.OUTPUT_TYPES.SH));
@@ -183,7 +178,7 @@ contract('ValidateSPV', () => {
       const output = '0x88080000000000001976a9141458514240d7287e5254af48cd292eb876cb07eb88ac';
       const value = new BN('0888', 16);
       const payload = '0x1458514240d7287e5254af48cd292eb876cb07eb';
-      const pkhOutput = await instance.parseOutput(output);
+      const pkhOutput = await ValidateSPV.parseOutput(output);
 
       assert(pkhOutput._value.eq(value));
       assert(pkhOutput._outputType.eq(utils.OUTPUT_TYPES.PKH));
@@ -194,7 +189,7 @@ contract('ValidateSPV', () => {
   describe('#parseHeader', async () => {
     it('returns the header digest, version, prevHash, merkleRoot, timestamp, target, and nonce',
       async () => {
-        const validHeader = await instance.parseHeader(
+        const validHeader = await ValidateSPV.parseHeader(
           constants.OP_RETURN.INDEXED_HEADERS[0].HEADER
         );
 
@@ -212,57 +207,57 @@ contract('ValidateSPV', () => {
 
     it('bubble up errors if input header is not 80 bytes', async () => {
       // Removed a byte from the header version to create error
-      const invalidHeader = await instance.parseHeader(
+      const invalidHeader = await ValidateSPV.parseHeader(
         constants.HEADER_ERR.HEADER_0_LEN
       );
 
       assert.equal(constants.EMPTY, invalidHeader._digest);
-      assert(zeroBN.eq(invalidHeader._version));
+      assert(new BN('0', 10).eq(invalidHeader._version));
       assert.equal(constants.EMPTY, invalidHeader._prevHash);
       assert.equal(constants.EMPTY, invalidHeader._merkleRoot);
-      assert(zeroBN.eq(invalidHeader._timestamp));
-      assert(zeroBN.eq(invalidHeader._target));
-      assert(zeroBN.eq(invalidHeader._nonce));
+      assert(new BN('0', 10).eq(invalidHeader._timestamp));
+      assert(new BN('0', 10).eq(invalidHeader._target));
+      assert(new BN('0', 10).eq(invalidHeader._nonce));
     });
   });
 
   describe('#validateHeaderChain', async () => {
     it('returns true if header chain is valid', async () => {
-      const res = await instance.validateHeaderChain(constants.OP_RETURN.HEADER_CHAIN);
+      const res = await ValidateSPV.validateHeaderChain(constants.OP_RETURN.HEADER_CHAIN);
       assert(res.eq(new BN('49134394618239', 10)));
     });
 
     it('returns ERR_BAD_LENGTH if header chain is not divisible by 80', async () => {
-      const res = await instance.validateHeaderChain(constants.HEADER_ERR.HEADER_CHAIN_INVALID_LEN);
+      const res = await ValidateSPV.validateHeaderChain(constants.HEADER_ERR.HEADER_CHAIN_INVALID_LEN);
       assert(res.eq(new BN('ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 16)));
     });
 
     it('returns ERR_INVALID_CHAIN if header chain prevHash is invalid', async () => {
-      const res = await instance.validateHeaderChain(
+      const res = await ValidateSPV.validateHeaderChain(
         constants.HEADER_ERR.HEADER_CHAIN_INVALID_PREVHASH
       );
       assert(res.eq(new BN('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe', 16)));
     });
 
     it('returns ERR_LOW_WORK if a header does not meet its target', async () => {
-      const res = await instance.validateHeaderChain(constants.HEADER_ERR.HEADER_CHAIN_LOW_WORK);
+      const res = await ValidateSPV.validateHeaderChain(constants.HEADER_ERR.HEADER_CHAIN_LOW_WORK);
       assert(res.eq(new BN('fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffd', 16)));
     });
   });
 
   describe('#validateHeaderWork', async () => {
     it('returns false on an empty digest', async () => {
-      const res = await instance.validateHeaderWork(constants.EMPTY, 0);
+      const res = await ValidateSPV.validateHeaderWork(constants.EMPTY, 0);
       assert.isFalse(res);
     });
 
     it('returns false if the digest has insufficient work', async () => {
-      const res = await instance.validateHeaderWork('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 1);
+      const res = await ValidateSPV.validateHeaderWork('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff', 1);
       assert.isFalse(res);
     });
 
     it('returns true if the digest has sufficient work', async () => {
-      const res = await instance.validateHeaderWork(
+      const res = await ValidateSPV.validateHeaderWork(
         constants.OP_RETURN.INDEXED_HEADERS[0].DIGEST_BE,
         new BN('3840827764407250199942201944063224491938810378873470976', 10)
       );
@@ -272,7 +267,7 @@ contract('ValidateSPV', () => {
 
   describe('#validateHeaderPrevHash', async () => {
     it('returns true if header prevHash is valid', async () => {
-      const res = await instance.validateHeaderPrevHash(
+      const res = await ValidateSPV.validateHeaderPrevHash(
         constants.OP_RETURN.INDEXED_HEADERS[1].HEADER,
         constants.OP_RETURN.INDEXED_HEADERS[0].DIGEST_LE
       );
@@ -280,7 +275,7 @@ contract('ValidateSPV', () => {
     });
 
     it('returns false if header prevHash is invalid', async () => {
-      const res = await instance.validateHeaderPrevHash(
+      const res = await ValidateSPV.validateHeaderPrevHash(
         constants.OP_RETURN.INDEXED_HEADERS[1].HEADER,
         constants.OP_RETURN.INDEXED_HEADERS[1].DIGEST_LE
       );
