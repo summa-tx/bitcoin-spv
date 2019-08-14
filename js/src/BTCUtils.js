7df7fc7a7ec276f5c301739c67255b990c402071
James Prestwich <james@prestwi.ch>
2019-08-13 17:40:15 -0700

    chore: express a script opcode in hex for clarity

--- js/src/BTCUtils.js
+++ js/src/BTCUtils.js
@@ -428,7 +428,7 @@ export function extractValue(output) {
  * @throws {TypeError}    When passed something other than an op return output
  */
 export function extractOpReturnData(output) {
-  if (output[9] !== 106) {
+  if (output[9] !== 0x6a) {
     throw new TypeError('Malformatted data. Must be an op return.');
   }
   const dataLen = output[10];
