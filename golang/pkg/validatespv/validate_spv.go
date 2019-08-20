463bd29e76fade9b3695638d53d8800ee784d581
DJ <deejay.whitegrass@gmail.com>
2019-08-19 21:20:08 -0600

    Adds empty functions to validate_spv.go

--- golang/pkg/validatespv/validate_spv.go
+++ golang/pkg/validatespv/validate_spv.go
@@ -1,13 +1,46 @@
 package validatespv
 
 import (
-	// "bytes"
-	// "crypto/sha256"
-	// "encoding/binary"
-	// "encoding/hex"
-	// "errors"
-	// "math/big"
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"encoding/hex"
+	"errors"
+	"math/big"
 
-	// sdk "github.com/cosmos/cosmos-sdk/types"
-	// "golang.org/x/crypto/ripemd160"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"golang.org/x/crypto/ripemd160"
 )
+
+func prove(txid []byte, merkleRoot []byte, intermediateNodes []byte, index uint) bool {
+	// Shortcut the empty-block case
+	if
+}
+
+func calculateTxId() {
+
+}
+
+func parseInput() {
+
+}
+
+func parseOutput() {
+
+}
+
+func parseHeader() {
+
+}
+
+func validateHeaderWork() {
+
+}
+
+func validateHeaderPrevHash() {
+
+}
+
+func validateHeaderChain() {
+
+}
