e34bd0846438492b8efceb71e2a8fb5966e0624c
DJ <deejay.whitegrass@gmail.com>
2019-08-19 21:07:38 -0600

    Adds basically empty files for starting validatespv code

---
+++ golang/pkg/validatespv/validate_spv.go
@@ -0,0 +1,13 @@
+package validatespv
+
+import (
+	// "bytes"
+	// "crypto/sha256"
+	// "encoding/binary"
+	// "encoding/hex"
+	// "errors"
+	// "math/big"
+
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+	// "golang.org/x/crypto/ripemd160"
+)
---
+++ golang/pkg/validatespv/validate_spv_test.go
@@ -0,0 +1,15 @@
+package validatespv
+
+import (
+	// "bytes"
+	// "encoding/hex"
+	// "encoding/json"
+	// "io/ioutil"
+	// "log"
+	// "os"
+	// "testing"
+
+	// "github.com/stretchr/testify/suite"
+
+	// sdk "github.com/cosmos/cosmos-sdk/types"
+)
