a93e679c1a0c00aeb8f91a14df5fb94a22d82cb6
James Prestwich <james@prestwi.ch>
2019-10-02 16:42:24 -0700

    feature: convenience functions for encoding addresses

--- golang/btcspv/utils.go
+++ golang/btcspv/utils.go
@@ -2,6 +2,9 @@ package btcspv
 
 import (
 	"encoding/hex"
+
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/btcsuite/btcutil/bech32"
 )
 
 func strip0xPrefix(s string) string {
@@ -57,4 +60,35 @@ func GetInputType(num uint) string {
 		typeString = "Witness"
 	}
 	return typeString
+}
+
+// EncodeP2SH turns a scripthash into an address
+func EncodeP2SH(sh []byte) string {
+	return base58.CheckEncode(sh, 5)
+}
+
+// EncodeP2PKH turns a pubkey hash into an address
+func EncodeP2PKH(pkh []byte) string {
+	return base58.CheckEncode(pkh, 0)
+}
+
+func encodeSegWit(payload []byte, version int) string {
+	adj, err := bech32.ConvertBits(payload, 8, 5, true)
+	if err != nil {
+		return ""
+	}
+	combined := []byte{0x00}
+	combined = append(combined, adj...)
+	res, _ := bech32.Encode("bc", combined)
+	return res
+}
+
+// EncodeP2WSH turns a scripthash into an address
+func EncodeP2WSH(sh []byte) string {
+	return encodeSegWit(sh, 0)
+}
+
+// EncodeP2WPKH turns a pubkey hash into an address
+func EncodeP2WPKH(pkh []byte) string {
+	return encodeSegWit(pkh, 0)
 }
