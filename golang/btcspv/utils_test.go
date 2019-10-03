27c9e590fb026a2ed896cd97b842c298aa0a141e
Erin Hales <erin@summa.one>
2019-10-03 14:01:19 -0600

    Added most utils tests

--- golang/btcspv/utils_test.go
+++ golang/btcspv/utils_test.go
@@ -1,21 +1,139 @@
 package btcspv
 
-// import (
-// 	sdk "github.com/cosmos/cosmos-sdk/types"
-// )
+func (suite *UtilsSuite) TestDecodeIfHex() {
+	var expected []byte
+	var actual []byte
 
-// func (suite *UtilsSuite) TestProve() {
-// 	fixture := suite.Fixtures["prove"]
+	expected = []byte{0}
+	actual = DecodeIfHex("0x00")
+	suite.Equal(expected, actual)
 
-// 	for i := range fixture {
-// 		testCase := fixture[i]
-// 		expected := testCase.Output.(bool)
-// 		inputs := testCase.Input.(map[string]interface{})
-// 		txIDLE := inputs["txIdLE"].([]byte)
-// 		merkleRootLE := inputs["merkleRootLE"].([]byte)
-// 		proof := inputs["proof"].([]byte)
-// 		index := uint(inputs["index"].(int))
-// 		actual := Prove(txIDLE, merkleRootLE, proof, index)
-// 		suite.Equal(expected, actual)
+	expected = []byte{0}
+	actual = DecodeIfHex("00")
+	suite.Equal(expected, actual)
+
+	expected = []byte{0, 1, 2, 42, 100, 101, 102, 255}
+	actual = DecodeIfHex("0x0001022a646566ff")
+	suite.Equal(expected, actual)
+}
+
+func (suite *UtilsSuite) TestGetOutputType() {
+	var expected string
+	var actual string
+
+	expected = "Output None"
+	actual = GetOutputType(0)
+	suite.Equal(expected, actual)
+
+	expected = "WPKH"
+	actual = GetOutputType(1)
+	suite.Equal(expected, actual)
+
+	expected = "WSH"
+	actual = GetOutputType(2)
+	suite.Equal(expected, actual)
+
+	expected = "Op Return"
+	actual = GetOutputType(3)
+	suite.Equal(expected, actual)
+
+	expected = "PKH"
+	actual = GetOutputType(4)
+	suite.Equal(expected, actual)
+
+	expected = "SH"
+	actual = GetOutputType(5)
+	suite.Equal(expected, actual)
+
+	expected = "Nonstandard"
+	actual = GetOutputType(6)
+	suite.Equal(expected, actual)
+}
+
+func (suite *UtilsSuite) TestGetInputType() {
+	var expected string
+	var actual string
+
+	expected = "Input None"
+	actual = GetInputType(0)
+	suite.Equal(expected, actual)
+
+	expected = "Legacy"
+	actual = GetInputType(1)
+	suite.Equal(expected, actual)
+
+	expected = "Compatibility"
+	actual = GetInputType(2)
+	suite.Equal(expected, actual)
+
+	expected = "Witness"
+	actual = GetInputType(3)
+	suite.Equal(expected, actual)
+}
+
+func (suite *UtilsSuite) TestEncodeP2SH() {
+	fixture := suite.Fixtures["encodeP2SH"]
+
+	for i := range fixture {
+		testCase := fixture[i]
+
+		input := testCase.Input.([]byte)
+		expected := testCase.Output
+		actual := DecodeIfHex(EncodeP2SH(input))
+		suite.Equal(expected, actual)
+	}
+}
+
+func (suite *UtilsSuite) TestEncodeP2PKH() {
+	fixture := suite.Fixtures["encodeP2PKH"]
+
+	for i := range fixture {
+		testCase := fixture[i]
+
+		input := testCase.Input.([]byte)
+		expected := testCase.Output
+		actual := DecodeIfHex(EncodeP2PKH(input))
+		suite.Equal(expected, actual)
+	}
+}
+
+// func encodeSegWit(payload []byte, version int) string {
+// 	adj, err := bech32.ConvertBits(payload, 8, 5, true)
+// 	if err != nil {
+// 		return ""
 // 	}
+// 	combined := []byte{0x00}
+// 	combined = append(combined, adj...)
+// 	res, _ := bech32.Encode("bc", combined)
+// 	return res
 // }
+
+func (suite *UtilsSuite) TestEncodeSegWit() {
+
+}
+
+func (suite *UtilsSuite) TestEncodeP2WSH() {
+	fixture := suite.Fixtures["encodeP2WSH"]
+
+	for i := range fixture {
+		testCase := fixture[i]
+
+		input := testCase.Input.([]byte)
+		expected := testCase.Output
+		actual := DecodeIfHex(EncodeP2WSH(input))
+		suite.Equal(expected, actual)
+	}
+}
+
+func (suite *UtilsSuite) TestEncodeP2WPKH() {
+	fixture := suite.Fixtures["encodeP2WPKH"]
+
+	for i := range fixture {
+		testCase := fixture[i]
+
+		input := testCase.Input.([]byte)
+		expected := testCase.Output
+		actual := DecodeIfHex(EncodeP2WPKH(input))
+		suite.Equal(expected, actual)
+	}
+}
