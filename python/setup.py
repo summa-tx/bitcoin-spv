33f1bc243b946191bbb37901d72f37423097f659
James Prestwich <james@prestwi.ch>
2019-10-18 16:42:38 -0700

    chore: fix up setup.py

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,22 @@
+# flake8: noqa
 from setuptools import setup, find_packages
 
+reqs = [
+    'riemann-tx==2.0.1'
+]
+
 setup(
-    name='btcspv',
+    name='bitcoin-spv-py',
     version='0.0.1',
+    url='https://github.com/summa-tx/bitcoin-spv',
     author='James Prestwich',
-    license='LGPL',
+    author_email='james@summa.one',
+    install_requires=reqs,
     packages=find_packages(),
     package_data={'btcspv': ['py.typed']},
     package_dir={'btcspv': 'btcspv'},
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    classifiers=[
+        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)'
+    ]
 )
