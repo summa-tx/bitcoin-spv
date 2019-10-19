dbea4c6de91b1346fd3d3e6af3c7a994937ef359
James Prestwich <10149425+prestwich@users.noreply.github.com>
2019-10-18 17:01:54 -0700

    Py setup fixes (#73)
    * chore: fix up setup.py
    
    * chore: add new egginfo to gitignore
    
    * tests: add bad coverage for header (de)ser

--- python/btcspv/test/test_ser.py
+++ python/btcspv/test/test_ser.py
@@ -17,3 +17,11 @@ class TestSer(unittest.TestCase):
             second_proof = ser.deserialize_spv_proof(json_proof_string)
 
             self.assertEqual(proof, second_proof)
+
+    def test_deser_header_roundtrip(self):
+        for v in self.vectors['validHeader']:
+            header = ser.deserialize_relay_header(v)
+            json_header_string = ser.serialize_relay_header(header)
+            second_header = ser.deserialize_relay_header(json_header_string)
+
+            self.assertEqual(header, second_header)
