2b77c8b7398efa574d70d9a4255b4b5d66e42d57
Barbara Liau <barbara@liau.io>
2019-12-03 15:10:39 -0700

    Add test folder with placeholder test

---
+++ js/clients/test/BcoinClient-test.js
@@ -0,0 +1,9 @@
+const BcoinClient = require('../lib/BcoinClient');
+
+describe('BcoinClient', () => {
+  it('needs tests. this is a placeholder', () => {
+    const client = new BcoinClient({});
+
+    expect(client).toEqual(Object);
+  });
+});
