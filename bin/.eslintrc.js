e853551e42e6f4599ea2cba6d642cfb47c0e0a7b
James Prestwich <james@prestwi.ch>
2020-03-25 13:11:06 -0700

    bug: get the proofs CLI working again :)

---
+++ bin/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    "comma-dangle": 'off',
+    "no-bitwise": 'off'
+  },
+};
